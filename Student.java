49a06316a23f21fb7f99303931eb0e70e4ea3bd7
JaredKirsch20 <159041808+JaredKirsch20@users.noreply.github.com>
2024-04-08 19:06:08 -0400

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,1 +1,19 @@
+public class Student{
 
+    String name;
+    double average;
+
+    Student (String n, double a){
+        this.name = n;
+        this.average = a;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public double getAverage(){
+        return this.average;
+    }
+
+}
